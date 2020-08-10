7999220f2dfc4c656d2760bae64556ff1ddec255
root <root@ip-172-31-39-248.us-east-2.compute.internal>
2020-08-10 04:01:21 +0000

    first commit

---
+++ src/main/java/war/BootpackageApplication.java
@@ -0,0 +1,12 @@
+package war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootpackageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootpackageApplication.class, args);
+	}
+}
